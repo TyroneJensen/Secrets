df792e36dee7f6280523911293e5428eb3302a9b
Tyrone Jensen <56504946+drtjensen@users.noreply.github.com>
2020-11-02 06:25:50 +0200

    level 4 salting and hashing with bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require ("md5");
+const bcrypt = require ("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -40,24 +41,28 @@ app.get("/register", function(req, res){
 //define post routes
 // register user email and password in DB and allow access
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err){
+      if (err){
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 
-  newUser.save(function(err){
-    if (err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
 });
 
 // search DB for email(username) and if found verify with password and allow access
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne(
     {email: username},
@@ -65,8 +70,12 @@ app.post("/login", function(req,res){
       if (err) {
         console.log(err);
       } else {
-        if (foundUser.password === password){
-          res.render("secrets");
+        if (foundUser) {
+          bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
+              res.render("secrets");
+            }
+          });
         }
       }
     });

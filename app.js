b20a3709f5319b53cde2b745367d673ae71f31f3
Tyrone Jensen <56504946+drtjensen@users.noreply.github.com>
2021-05-24 23:21:14 +0200

    installed packages for cookies and sessions, implemented passport local startegy

--- app.js
+++ app.js
@@ -1,86 +1,123 @@
 //jshint esversion:6
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require ("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//DB setup
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+//setup express-session
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-const userSchema = new mongoose.Schema ({
-  email: String,
-  password: String
+//config passport
+app.use(passport.initialize());
+//config passport with express-session
+app.use(passport.session());
+
+//DB setup
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+//needed fix for passportLocalMongoose deprecation issues
+mongoose.set("useCreateIndex", true);
 
+//Schema setup
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+//config passportLocalMongoose
+userSchema.plugin(passportLocalMongoose);
+
+// mongoose setup
 const User = mongoose.model("User", userSchema);
 
+//config passport-local strategy
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // define get routes
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function (req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
 //define post routes
-// register user email and password in DB and allow access
-app.post("/register", function(req, res){
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-
-    newUser.save(function(err){
-      if (err){
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
         console.log(err);
+        res.redirect("/register");
       } else {
-        res.render("secrets");
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
       }
-    });
-  });
-
+    }
+  );
 });
 
 // search DB for email(username) and if found verify with password and allow access
-app.post("/login", function(req,res){
-  const username = req.body.username;
-  const password = req.body.password;
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
 
-  User.findOne(
-    {email: username},
-    function(err, foundUser){
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser) {
-          bcrypt.compare(password, foundUser.password, function(err, result) {
-            if (result === true) {
-              res.render("secrets");
-            }
-          });
-        }
-      }
-    });
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
+  });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
